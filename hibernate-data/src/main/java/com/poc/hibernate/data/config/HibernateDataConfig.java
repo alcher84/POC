36bcb719f937e97b63e2a124b3089fa47951291b
unknown <alcher84@gmail.com>
2015-10-22 14:50:13 +0300

    Added hibernate module

---
+++ hibernate-data/src/main/java/com/poc/hibernate/data/config/HibernateDataConfig.java
@@ -0,0 +1,10 @@
+package com.poc.hibernate.data.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by achernysh on 22.10.2015.
+ */
+@Configuration
+public class HibernateDataConfig {
+}
